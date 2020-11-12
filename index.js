b2c59417f30e4a2c0bb0c4f5ad8320848edd2410
kevinneyer <kevinneyer@gmail.com>
2020-11-12 14:28:17 -0500

    add and sort tests complete

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const sum = (array) => {
-   return array.reduce((a,b) => (a + b), 0)
-}
+ module.exports = {
+    sum: function(array) {
+        return array.reduce((a,b) => (a + b), 0)
+    },
+    sort: function(array) {
+        return array.sort((a,b) => a < b ? -1 : 1)
+    }
+ }
--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,22 @@
-const { expect } = require("chai")
+const  expect  = require("chai").expect
+const assert = require("chai").assert
+const sum = require('../index.js').sum
+const sort = require('../index.js').sort
 
-describe ('#sum()', function() {
-    it('adds all the elements of an array', function () {
-        expect(sum([1,2,3,4,5]).to.equal(15))
+
+describe ('sum', function(){
+    it('should return the sum of all elements', function(){
+        let array = [1,2,3,4,5]
+        expect(sum(array)).to.equal(15)
+    });
+    it('should return an integer', function(){
+        expect(typeof sum([1,2,3,4,5])).to.equal('number')
     })
 }) 
+
+describe('sort', function(){
+    it('should sort an array from low to high', function(){
+        let result = sort([3,1,4,2,5])
+        assert(result, [1,2,3,4,5])
+    })
+})
